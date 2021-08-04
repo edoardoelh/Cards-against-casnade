10823e5b017cd0649db5f318e63e32376ef4c619
Eddie <edoardoelh@gmail.com>
2021-08-04 17:13:26 +0200

    create load page system

--- js/front/index.js
+++ js/front/index.js
@@ -1,35 +1,3 @@
 $(document).ready(function () {
-    var tag_join_visible = false;
-
-    $("#btn_index_join_room").click(function () {
-        $("#btn_index_back").removeClass("collapse");
-        $("#btn_index_join_private_room").removeClass("collapse");
-        $("#btn_index_join_public_room").removeClass("collapse");
-        $("#btn_index_create_room").addClass("collapse");
-        $("#btn_index_join_room").addClass("collapse");
-    });
-
-    $("#btn_index_back").click(function () {
-        if (tag_join_visible == true){
-            $("[tag='private_room']").addClass("collapse");
-            tag_join_visible = false
-        }else{
-            console.log("entra aqui");
-            $("#btn_index_back").addClass("collapse");
-            $("[tag='join']").addClass("collapse");
-            $("[tag='main']").removeClass("collapse");
-        }
-        
-    });
-
-    $("#btn_index_join_private_room").click(function () {
-        $("[tag='private_room']").toggleClass("collapse");
-        tag_join_visible = true
-    });
-
-
-    function load_new_page(id, fileRoute) {
-        document.getElementById("content").innerHTML='<object type="text/html" data="' + fileRoute + '" ></object>';
-   }
-
+    loadNewPage("mainPage");
 });
